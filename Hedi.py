13c503a72d19ef912e5749710087db4983567775
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 20:17:53 +0100

    Update Hedi.py

--- Hedi.py
+++ Hedi.py
@@ -1,16 +1,5 @@
-import platform
-import os
-os.system('termux-setup-storage')
-os.system('git pull')
+import os, sys,platform
 try:
- os.system('mkdir result')
-try:
- os.system('touch ua.txt')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("IGHEDI64").login_kamu()
-elif 'aarch' in arc:
-	__import__("IGHEDI32").login_kamu()
-else:
-	exit(f' Unknown device machine {arc}')
+    __import__("IGHEDI64").login_kamu()
+except Exception as e:
+    exit(str(e))
