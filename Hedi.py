0449f6333046efa5865fd7d9673a4cde637548c8
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 20:16:18 +0100

    Update Hedi.py

--- Hedi.py
+++ Hedi.py
@@ -2,7 +2,7 @@ import platform
 import os
 os.system('termux-setup-storage')
 os.system('git pull')
-try:os.system('mkdir result)
+try:os.system('mkdir result')
 try:os.system('touch ua.txt')
 except:pass
 arc = str(platform.uname().machine)
