6d468c176cdaab0ba236a64b15388e3f660c90ff
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 20:15:52 +0100

    Create Hedi.py

---
+++ Hedi.py
@@ -0,0 +1,14 @@
+import platform
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir result)
+try:os.system('touch ua.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("IGHEDI64").login_kamu()
+elif 'aarch' in arc:
+	__import__("IGHEDI32").login_kamu()
+else:
+	exit(f' Unknown device machine {arc}')
