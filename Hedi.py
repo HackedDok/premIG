caee42cba5c497a1f222237705d7e79067679d43
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 20:17:09 +0100

    Update Hedi.py

--- Hedi.py
+++ Hedi.py
@@ -3,9 +3,9 @@ import os
 os.system('termux-setup-storage')
 os.system('git pull')
 try:
-os.system('mkdir result')
+ os.system('mkdir result')
 try:
-os.system('touch ua.txt')
+ os.system('touch ua.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
