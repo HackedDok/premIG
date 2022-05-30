a40468de62cf275113efc244679ffd45a5e62f04
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 22:10:09 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,14 +1,1 @@
 
-import platform
-import os
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('touch ua.txt')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("IGHEDI").license()
-elif 'aarch' in arc:
-	__import__("IGHEDI").login_kamu()
-else:
-	exit(f' Unknow device machine {arc}')
