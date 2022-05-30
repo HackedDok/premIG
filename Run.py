50a3ae22ee21c1f7a9c1e66a62687f2b0acd6194
Hedi Gader <94740559+HackedDok@users.noreply.github.com>
2022-05-30 22:07:30 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -7,8 +7,8 @@ try:os.system('touch ua.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("IGHEDI").license()
-elif 'aarch' in arc:
 	__import__("IGHEDI").login_kamu()
+elif 'aarch' in arc:
+	__import__("IGHEDI").license()
 else:
 	exit(f' Unknow device machine {arc}')
